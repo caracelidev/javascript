644de6f28a1c484c2138c4e7b54ea1a032170fbc
caraceli <lincoln.caraceli@gmail.com>
2024-04-15 16:27:25 -0300

    att

---
+++ desafios/d3.js
@@ -0,0 +1,7 @@
+/* 03)
+ Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente */
+
+ function baseExpoente(a, b){
+    return a ** b
+ }
+ console.log(baseExpoente(20,2))
