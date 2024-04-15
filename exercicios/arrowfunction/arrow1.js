5ff94e0a790ef5e5e0158937e779853f5cd7dad7
caraceli <lincoln.caraceli@gmail.com>
2024-04-15 12:48:55 -0300

    att

---
+++ exercicios/arrowfunction/arrow1.js
@@ -0,0 +1,16 @@
+let dobro = function(a){
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+// Se a função tiver apenas 1 PARÂMETRO, pode retirar o parênteses //
+
+dobro = a => 2 * a
+
+let ola = function(){
+    return "olá"
+}
+
+ola = () => "olá"
