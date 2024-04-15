e857534fff0dd2d35df4e7c0687e4520cc8a5747
caraceli <lincoln.caraceli@gmail.com>
2024-04-15 15:04:01 -0300

    att

---
+++ desafios/d1.js
@@ -0,0 +1,19 @@
+/* 1) Crie uma função que dado dois valores (passados como parâmetros)
+ mostre no console a soma, subtração, multiplicação e divisão desses valores.*/
+
+ function ssmd(x, y) {
+    // Soma
+    console.log("Soma:", x + y)
+
+    // Subtração
+    console.log("Subtração:", x - y)
+
+    // Multiplicação
+    console.log("Multiplicação:", x * y)
+
+    // Divisão
+    console.log("Divisão:", x / y) 
+}
+
+// Exemplo de uso da função
+ssmd(10, 5);
