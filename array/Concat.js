acd140bfce017be22c678b6d2786db5f1f4f2791
caraceli <lincoln.caraceli@gmail.com>
2024-04-18 14:08:26 -0300

    att

---
+++ array/Concat.js
@@ -0,0 +1,7 @@
+const filhos1 = ['Jhonson', 'Clébinho', 'Lincoln']
+const filhos2 = ['Juan', 'Pedro', 'Iago']
+const todos = filhos1.concat(filhos2)
+
+console.log(todos)
+console.log(filhos1)
+console.log(filhos2)
