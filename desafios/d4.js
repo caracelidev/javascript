e8fb5905081da4580a6d1f7535816a7f320849e8
caraceli <lincoln.caraceli@gmail.com>
2024-04-19 19:00:26 -0300

    att

---
+++ desafios/d4.js
@@ -0,0 +1,41 @@
+nomeDoMes = (mes) => {
+    
+
+    if (mes == 1){
+        return 'Janeiro'
+    }
+    else if(mes == 2){
+        return 'Fevereiro'
+    }
+    else if(mes == 3){
+        return 'Março'
+    }
+    else if(mes == 4){
+        return 'Abril'
+    }
+    else if(mes == 5){
+        return 'Maio'
+    }
+    else if(mes == 6){
+        return 'Junho'
+    }
+    else if(mes == 7){
+        return 'Julho'
+    }
+    else if(mes == 8){
+        return 'Agosto'
+    }
+    else if(mes == 9){
+        return 'Setembro'
+    }
+    else if(mes == 10){
+        return 'Outubro'
+    }
+    else if(mes == 11){
+        return 'Novembro'
+    }
+    else{
+        return 'Dezembro'
+    }
+}
+console.log(nomeDoMes(5))
