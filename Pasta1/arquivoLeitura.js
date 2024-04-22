84169fdd005acf4a8206a205654447046938f466
caraceli <lincoln.caraceli@gmail.com>
2024-04-22 13:25:24 -0300

    att

--- Pasta1/arquivoLeitura.js
+++ Pasta1/arquivoLeitura.js
@@ -1,1 +1,15 @@
-const fs = 
+const fs = require('fs')
+const caminho = __dirname + '/arquivo.json'
+
+// Síncrono //
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+console.log(conteudo)
+
+// Assíncrono // 
+fs.readFile(caminho, 'UTF-8', (err, conteudo) => { 
+    const config = JSON.parse(conteudo)
+    console.log(`${config.db.host}: ${config.db.port}`)
+})
+
+const config = require('./arquivo.json')
+console.log(config.db)
