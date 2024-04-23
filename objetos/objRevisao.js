1afc45d7d192d1cfa06f7a049248c8478ff0aeb6
caraceli <lincoln.caraceli@gmail.com>
2024-04-23 14:56:48 -0300

    att

---
+++ objetos/objRevisao.js
@@ -0,0 +1,14 @@
+const pessoa1 = new Object()
+pessoa1.nome = 'Lincoln'
+pessoa1.sobrenome = 'Caraceli'
+pessoa1.idade = 18
+pessoa1.cidade = 'Limeira'
+console.log(pessoa1)
+
+const pessoa2 = {
+    nome: 'Lego',
+    sobrenome:'Ney',
+    idade: 24,
+    cidade: 'Serra Negra'
+}
+console.log(pessoa2)
