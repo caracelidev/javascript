599f9e1b8c1132f250c514ddd207a9050a36ac1f
caraceli <lincoln.caraceli@gmail.com>
2024-04-24 00:50:54 -0300

    att

--- OperacaoTernaria.js
+++ OperacaoTernaria.js
@@ -3,5 +3,5 @@ const pontuacaoUsuario = 1001
 /*if (pontuacaoUsuario >= 1000) {return 'Usuário Vip'}
 else {return 'Usuário normal'}*/
 
-const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário Vip' : 'Usuário normal'
+const nivelUsuario = (pontuacaoUsuario) >= 1000 ? 'Usuário Vip' : 'Usuário normal'
 console.log(nivelUsuario)
