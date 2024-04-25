28ddbfd94f064fed8c5c5e807d64aec332ad985e
caraceli <lincoln.caraceli@gmail.com>
2024-04-25 17:42:10 -0300

    att

--- es6/promises.js
+++ es6/promises.js
@@ -1,23 +1,23 @@
 // Promise só aceita 1 parâmetro //
 
-const promisse1 = new Promise(function(cumprirPromessa){
+const promisse1 = new Promise((cumprirPromessa) => {
     cumprirPromessa(3)
 })
 
-promisse1.then(function(valor){
+promisse1.then((valor) => {
     console.log(valor)
 })
 
 // Promise aceita mais parâmetros se abrir as chaves //
 
-const promisse2 = new Promise(function(cumprirPromessa){
+const promisse2 = new Promise((cumprirPromessa) => {
     cumprirPromessa({
         x: 5,
         y: 2
     })
 })
 
-promisse2.then(function(valor){
+promisse2.then((valor) => {
     console.log(valor)
 })
 
--- es6/promises5.js
+++ es6/promises5.js
@@ -1,9 +1,35 @@
-function esperarPor(tempo = 2000){
+function esperarPor (tempo = 2000) {
     return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log('Executando promise...')
-            resolve()
-        }, tempo)
+        setTimeout(() => resolve(), tempo)
     })
 }
-esperarPor()
+
+esperarPor(3000)
+    .then(() => console.log('Executando promise 1...'))
+    .then(esperarPor)
+    .then(() => console.log('Executando promise 2...'))
+    .then(esperarPor)
+    .then(() => console.log('Executando promise 3...'))
+
+function retornarValor() {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(10), 5000)
+    })
+}
+
+async function executar(){
+    let valor = await retornarValor()
+
+    await esperarPor(2000)
+    console.log(`Async/Await ${valor}...`)
+
+    await esperarPor(2000)
+    console.log(`Async/Await ${valor + 1}...`)
+
+    await esperarPor(2000)
+    console.log(`Async/Await ${valor + 2}...`)
+
+    return valor + 3
+}
+
+executar().then(console.log)
