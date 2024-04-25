74535c8bc7709919fa8f92fda44f219ab117e329
caraceli <lincoln.caraceli@gmail.com>
2024-04-25 19:33:53 -0300

    att

--- array/reduce.js
+++ array/reduce.js
@@ -2,7 +2,7 @@
 
 const numeros = [5, 50, 80, 1, 2 ,3, 5, 8, 7, 11, 15, 22, 27]
 
-const total = numeros.reduce(function(acumulador, valor){
+const total = numeros.reduce((acumulador, valor) => {
     return acumulador += valor
 })
 
